b43d2ab3f1307a36a2d3e78a358658aec8eb7d51
xxracer <maijelcancines2@gmail.com>
2026-04-25 15:24:38 -0400

    done

--- next.config.ts
+++ next.config.ts
@@ -1,54 +1,58 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  outputFileTracing: false,
+  experimental: {
+    // Fix for Next.js 15.5 bug with route groups in Vercel
+    // See: https://github.com/vercel/next.js/issues/71572
+    serverSourceMaps: false,
+  },
   images: {
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'cdn-s.acuityscheduling.com',
         port: '',
-        pathname: '/**', // Allows any path under this hostname
+        pathname: '/**',
       },
       {
         protocol: 'https',
-        hostname: 'placehold.co', // For placeholder images
+        hostname: 'placehold.co',
         port: '',
         pathname: '/**',
       },
       {
         protocol: 'https',
-        hostname: 'static.wixstatic.com', // For other static images used
+        hostname: 'static.wixstatic.com',
         port: '',
         pathname: '/**',
       },
       {
         protocol: 'https',
-        hostname: 'lh3.googleusercontent.com', // For Google user profile pictures in reviews
+        hostname: 'lh3.googleusercontent.com',
         port: '',
         pathname: '/**',
       },
       {
         protocol: 'https',
-        hostname: 'musclebeachclassic.com', // Sponsor logo
+        hostname: 'musclebeachclassic.com',
         port: '',
         pathname: '/**',
       },
       {
         protocol: 'https',
-        hostname: 'officialalphaland.com', // Sponsor logo
+        hostname: 'officialalphaland.com',
         port: '',
         pathname: '/**',
       },
       {
         protocol: 'https',
-        hostname: 'summershredding.com', // Sponsor logo
+        hostname: 'summershredding.com',
         port: '',
         pathname: '/**',
       },
       {
         protocol: 'https',
-        hostname: 'cdn.shopify.com', // Shopify CDN for sponsor logos
+        hostname: 'cdn.shopify.com',
         port: '',
         pathname: '/**',
       },
