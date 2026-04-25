f6e40289c2965467ef2c86aac467df726294a7d7
xxracer <maijelcancines2@gmail.com>
2026-04-25 15:16:10 -0400

    json

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'standalone',
+  outputFileTracing: false,
   images: {
     remotePatterns: [
       {
